994d43f317ee6132ece8be582a4a0b85ac201c18
João Pedro Alves <jotalmeida007@hotmail.com>
2023-06-02 21:31:05 -0300

    chore: remove console.log

--- backend/src/domain/services/tasks/update.js
+++ backend/src/domain/services/tasks/update.js
@@ -1,7 +1,6 @@
 import { updateTaskRepository } from '../../../database/repositories/task_db.js';
 
 const update = (params) => {
-  console.log(params);
   const updatedTask = updateTaskRepository(params);
 
   return updatedTask;
